9b72354ce445d2db7d8dec8bdbce1e402909a2e8
Greg Borenstein <greg@mfdz.com>
2009-02-10 14:42:36 -0800

    add rel=alternate check for atom entry urls

--- lib/feedzirra/atom_entry.rb
+++ lib/feedzirra/atom_entry.rb
@@ -3,7 +3,7 @@ module Feedzirra
     include SAXMachine
     include FeedEntryUtilities
     element :title
-    element :link, :as => :url, :value => :href, :with => {:type => "text/html"}
+    element :link, :as => :url, :value => :href, :with => {:type => "text/html", :rel => "alternate"}
     element :name, :as => :author
     element :content
     element :summary
