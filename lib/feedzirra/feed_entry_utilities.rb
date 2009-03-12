0f72cb92bc91cc8cf820c57812a7f8b409196f0d
Julien Genestoux <julien.genestoux@gmail.com>
2009-03-11 21:04:05 -0700

    adding smarter setters for published and updated that will keep the oldest published and the youngest updated

--- lib/feedzirra/feed_entry_utilities.rb
+++ lib/feedzirra/feed_entry_utilities.rb
@@ -18,12 +18,18 @@ module Feedzirra
       DateTime.parse(string).feed_utils_to_gm_time
     end
     
+    ##
+    # Writter for published. By default, we keep the "oldest" publish time found.
     def published=(val)
-      @published = parse_datetime(val)
+      parsed = parse_datetime(val)
+      @published = parsed if !@published || parsed < @published
     end
     
+    ##
+    # Writter for udapted. By default, we keep the most recenet update time found.
     def updated=(val)
-      @updated = parse_datetime(val)
+      parsed = parse_datetime(val)
+      @updated = parsed if !@updated || parsed > @updated
     end
     
     def content
