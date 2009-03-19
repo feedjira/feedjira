6baaba23d0d93e65049113bde5cc6b40679e318c
Paul Dix <paul@pauldix.net>
2009-03-19 10:33:58 -0400

    fixed bug where feed.sanitize! would error out if a value was nil

--- lib/feedzirra/feed_entry_utilities.rb
+++ lib/feedzirra/feed_entry_utilities.rb
@@ -11,9 +11,10 @@ module Feedzirra
     end
 
     def sanitize!
-      self.title.sanitize!
-      self.author.sanitize!
-      self.content.sanitize!
+      self.title.sanitize!   if self.title
+      self.author.sanitize!  if self.author
+      self.summary.sanitize! if self.summary
+      self.content.sanitize! if self.content
     end
     
     alias_method :last_modified, :published
