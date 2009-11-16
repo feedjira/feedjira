882acfa3e9cd7567d3652ba59859308d2b20736e
Paul Dix <paul@pauldix.net>
2009-11-16 09:17:45 -0500

    removed 'description' from the list of updatable attributes. not sure why that was in carson's commit

--- lib/feedzirra/feed_utilities.rb
+++ lib/feedzirra/feed_utilities.rb
@@ -1,7 +1,7 @@
 module Feedzirra
   module FeedUtilities
-    UPDATABLE_ATTRIBUTES = %w(title feed_url url last_modified etag description)
-    
+    UPDATABLE_ATTRIBUTES = %w(title feed_url url last_modified etag)
+
     attr_writer   :new_entries, :updated, :last_modified
     attr_accessor :etag
 
@@ -11,43 +11,43 @@ module Feedzirra
         entry ? entry.published : nil
       end
     end
-    
+
     def updated?
       @updated
     end
-    
+
     def new_entries
       @new_entries ||= []
     end
-    
+
     def has_new_entries?
       new_entries.size > 0
     end
-    
+
     def update_from_feed(feed)
       self.new_entries += find_new_entries_for(feed)
       self.entries.unshift(*self.new_entries)
-      
+
       @updated = false
       UPDATABLE_ATTRIBUTES.each do |name|
         @updated ||= update_attribute(feed, name)
       end
     end
-    
+
     def update_attribute(feed, name)
       old_value, new_value = send(name), feed.send(name)
-      
+
       if old_value != new_value
         send("#{name}=", new_value)
       end
     end
-    
+
     def sanitize_entries!
       entries.each {|entry| entry.sanitize!}
     end
-    
+
     private
-    
+
     def find_new_entries_for(feed)
       # this implementation is a hack, which is why it's so ugly.
       # it's to get around the fact that not all feeds have a published date.
@@ -63,7 +63,7 @@ module Feedzirra
       end
       found_new_entries
     end
-    
+
     def existing_entry?(test_entry)
       entries.any? { |entry| entry.url == test_entry.url }
     end
