f2467dc5c5debb7f5f012aeadb56400c92d54207
Paul Dix <paul@pauldix.net>
2010-03-04 10:01:17 -0500

    fixed active_support require to be compatible with rails 3

--- lib/feedzirra.rb
+++ lib/feedzirra.rb
@@ -1,15 +1,19 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
 
-gem 'activesupport'
-
 require 'zlib'
 require 'curb'
 require 'sax-machine'
 require 'loofah'
 require 'uri'
+
+require 'active_support/version'
 require 'active_support/basic_object'
 require 'active_support/core_ext/object'
-require 'active_support/core_ext/time'
+if ActiveSupport::VERSION::MAJOR >= 3
+  require 'active_support/time'
+else
+  require 'active_support/core_ext/time'
+end
 
 require 'core_ext/date'
 require 'core_ext/string'
