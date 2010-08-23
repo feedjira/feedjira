8e22dd57ff6741e82a283ee2949f2ae754ec980c
Tom Meier <ozmeier@yahoo.co.uk>
2010-08-23 15:40:08 +1000

    Fix date parsing to use in_time_zone parameter

--- lib/feedzirra/feed_entry_utilities.rb
+++ lib/feedzirra/feed_entry_utilities.rb
@@ -1,31 +1,31 @@
 module Feedzirra
-  module FeedEntryUtilities    
+  module FeedEntryUtilities
     def published
       @published || @updated
     end
-    
+
     def parse_datetime(string)
       begin
-        DateTime.parse(string).feed_utils_to_gm_time
+        DateTime.parse(string).in_time_zone
       rescue
         puts "DATE CAN'T BE PARSED: #{string}"
         nil
       end
     end
-    
+
     ##
     # Returns the id of the entry or its url if not id is present, as some formats don't support it
     def id
       @entry_id || @url
     end
-    
+
     ##
     # Writter for published. By default, we keep the "oldest" publish time found.
     def published=(val)
       parsed = parse_datetime(val)
       @published = parsed if !@published || parsed < @published
     end
-    
+
     ##
     # Writter for udapted. By default, we keep the most recenet update time found.
     def updated=(val)
@@ -39,7 +39,7 @@ module Feedzirra
       self.summary.sanitize! if self.summary
       self.content.sanitize! if self.content
     end
-    
+
     alias_method :last_modified, :published
   end
 end
