d6735d55630d4be1cf6a8934a2b4d191b808719c
Aditya Chadha <aditya@sublucid.com>
2010-10-18 12:13:01 -0400

    FeedBurner Atom now calls its entries items

--- lib/feedzirra/parser/atom_feed_burner.rb
+++ lib/feedzirra/parser/atom_feed_burner.rb
@@ -15,7 +15,7 @@ module Feedzirra
       element :title
       element :link, :as => :url, :value => :href, :with => {:type => "text/html"}
       element :link, :as => :feed_url, :value => :href, :with => {:type => "application/atom+xml"}
-      elements :entry, :as => :entries, :class => AtomFeedBurnerEntry
+      elements :item, :as => :entries, :class => AtomFeedBurnerEntry
 
       def self.able_to_parse?(xml) #:nodoc:
         (xml =~ /Atom/ && xml =~ /feedburner/) || false
