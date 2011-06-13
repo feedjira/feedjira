d35cefd73885ebb70de8f56fb3f47e9440422303
James Herdman <james.herdman@me.com>
2011-06-13 15:23:59 -0400

    Remove version check on ActiveSupport

--- lib/feedzirra.rb
+++ lib/feedzirra.rb
@@ -11,12 +11,7 @@ require 'active_support/basic_object'
 require 'active_support/core_ext/module'
 require 'active_support/core_ext/kernel'
 require 'active_support/core_ext/object'
-
-if ActiveSupport::VERSION::MAJOR >= 3
-  require 'active_support/time'
-else
-  require 'active_support/core_ext/time'
-end
+require 'active_support/time'
 
 require 'core_ext/date'
 require 'core_ext/string'
