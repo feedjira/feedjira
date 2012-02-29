a2014bb8b9f0c6a4ca29407414e149ad2e15778b
Brad Heller <brad.heller@gmail.com>
2012-02-29 15:26:27 -0800

    Checkpoint, trying something interesting.

---
+++ lib/feedzirra/parser/google_docs_atom_entry.rb
@@ -0,0 +1,26 @@
+module Feedzirra
+  module Parser
+    class GoogleDocsAtomEntry
+      include SAXMachine
+      include FeedEntryUtilities
+      
+      element :title
+      element :link, :as => :url, :value => :href, :with => {:type => "text/html", :rel => "alternate"}
+      element :name, :as => :author
+      element :content
+      element :summary
+      element :published
+      element :id, :as => :entry_id
+      element :created, :as => :published
+      element :issued, :as => :published
+      element :updated
+      element :modified, :as => :updated
+      elements :category, :as => :categories, :value => :term
+      elements :link, :as => :links, :value => :href
+
+      def url
+        @url ||= links.first
+      end
+    end
+  end
+end
