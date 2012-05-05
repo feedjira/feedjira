893c7942c0a697eab8eda47084cce468ea65ddfa
Filipe Pina <filipe.c.pina@gmail.com>
2012-05-06 00:03:38 +0100

    added image to RSSFeedBurnerEntry

--- lib/feedzirra/parser/rss_feed_burner_entry.rb
+++ lib/feedzirra/parser/rss_feed_burner_entry.rb
@@ -16,6 +16,8 @@ module Feedzirra
         element :"content:encoded", :as => :content
         element :description, :as => :summary
 
+        element :"media:content", :as => :image, :value => :url
+
         element :pubDate, :as => :published
         element :pubdate, :as => :published
         element :"dc:date", :as => :published
