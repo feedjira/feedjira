5f69015e7000354fc393c7b484237f4cd9adbdf6
matt swanson <mdswanson@sep.com>
2013-04-21 14:16:14 -0300

    Update rss.rb

--- lib/feedzirra/parser/rss.rb
+++ lib/feedzirra/parser/rss.rb
@@ -9,6 +9,7 @@ module Feedzirra
       element :description
       element :link, :as => :url
       elements :item, :as => :entries, :class => RSSEntry
+      element :published
 
       attr_accessor :feed_url
 
