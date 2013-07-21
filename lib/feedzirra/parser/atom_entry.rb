20720f52173113bd01b5d47d446cabe304c9cf1c
Patrick Gibson <patrick@patrickg.com>
2013-07-21 13:14:58 -0700

    Make use of the AtomLink class. #url and #feed_url pull values using the new #link method. The xml:base= attribute of the <feed> definition is also now considered if defined in the feed.

--- lib/feedzirra/parser/atom_entry.rb
+++ lib/feedzirra/parser/atom_entry.rb
@@ -5,12 +5,14 @@ module Feedzirra
     class AtomEntry
       include SAXMachine
       include FeedEntryUtilities
-      
+
       element :title
-      element :link, :as => :url, :value => :href, :with => {:type => "text/html", :rel => "alternate"}
       element :name, :as => :author
       element :content
       element :summary
+
+      elements :link, :as => :atom_links, :class => AtomLink
+      attribute :"xml:base", :as => :base
 
       element :"media:content", :as => :image, :value => :url
       element :enclosure, :as => :image, :value => :href
@@ -22,13 +24,53 @@ module Feedzirra
       element :updated
       element :modified, :as => :updated
       elements :category, :as => :categories, :value => :term
-      elements :link, :as => :links, :value => :href
 
       def url
-        @url ||= links.first
+        uri_join(base, link.href)
+      end
+
+      def url=(val)
+        @url = val
+      end
+
+      def replies_url
+        puts link(:replies).href
+        @replies_url ||= uri_join(base, link(:replies).href)
+      end
+
+      def replies_feed_url
+        @replies_feed_url ||= uri_join(base, link(:replies, 'application/atom+xml').href)
+      end
+
+      def link(rel = :alternate, type = false)
+        the_link = atom_links.select do |l|
+          l if l.rel == rel && (type ? l.type == type : true)
+        end.first
+
+        if !the_link
+          the_link = AtomLink.new
+          the_link.rel = rel
+          atom_links << the_link
+        end
+
+        the_link
+      end
+
+      def links
+        atom_links.map { |m| m.href }
+      end
+
+      private
+
+      def uri_join(part1, part2)
+        return nil if (part1.nil? && part2.nil?) || part2.nil?
+
+        if part1 && part1.match(/:\/\//)
+          URI.join(part1, part2).to_s
+        else
+          ((part1 + part2) if !part1.nil?) or part2
+        end
       end
     end
-
   end
-
 end
