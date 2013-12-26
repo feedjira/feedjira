e614278a8f00d7092c5ba9e1757ec460f782234f
Brad Cantin <bradcantin@collectivebias.com>
2013-12-26 07:34:35 -0600

    Bugfix for parsing dates that are ISO 8601 with milliseconds

--- spec/feedzirra/feed_entry_utilities_spec.rb
+++ spec/feedzirra/feed_entry_utilities_spec.rb
@@ -13,6 +13,12 @@ describe Feedzirra::FeedUtilities do
       time.class.should == Time
       time.should == Time.parse_safely("Wed Feb 20 18:05:00 UTC 2008")
     end
+
+    it "should parse a ISO 8601 with milliseconds into Time" do
+      time = @klass.new.parse_datetime("2013-09-17T08:20:13.931-04:00")
+      time.class.should == Time
+      time.should == Time.parse_safely("Tue Sep 17 12:20:13 UTC 2013")
+    end
   end
 
   describe "sanitizing" do
