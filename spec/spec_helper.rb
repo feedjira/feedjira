f3b09403cfb1e8b9cb2f16e3a335f8ccda02c45d
Jon Allured <jon.allured@gmail.com>
2014-03-17 18:24:20 -0500

    Remove unnecessary Rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,2 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/feedjira')
-
 require 'sample_feeds'
-
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-end
