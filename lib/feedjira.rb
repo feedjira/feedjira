4e2a9e699dc50b1650ee3b1903a9acf360450a7f
Jon Allured <jon.allured@gmail.com>
2014-03-20 07:39:43 -0500

    Don't autoload

--- lib/feedjira.rb
+++ lib/feedjira.rb
@@ -4,13 +4,26 @@ require 'sax-machine'
 require 'loofah'
 
 require 'feedjira/core_ext'
+require 'feedjira/feed_entry_utilities'
+require 'feedjira/feed_utilities'
+require 'feedjira/feed'
+require 'feedjira/parser'
+require 'feedjira/parser/rss_entry'
+require 'feedjira/parser/rss'
+require 'feedjira/parser/atom_entry'
+require 'feedjira/parser/atom'
 require 'feedjira/version'
 
-module Feedjira
-  autoload :FeedEntryUtilities, 'feedjira/feed_entry_utilities'
-  autoload :FeedUtilities,      'feedjira/feed_utilities'
-  autoload :Feed,               'feedjira/feed'
-  autoload :Parser,             'feedjira/parser'
+require 'feedjira/parser/rss_feed_burner_entry'
+require 'feedjira/parser/rss_feed_burner'
+require 'feedjira/parser/itunes_rss_owner'
+require 'feedjira/parser/itunes_rss_item'
+require 'feedjira/parser/itunes_rss'
+require 'feedjira/parser/atom_feed_burner_entry'
+require 'feedjira/parser/atom_feed_burner'
+require 'feedjira/parser/google_docs_atom_entry'
+require 'feedjira/parser/google_docs_atom'
 
+module Feedjira
   class NoParserAvailable < StandardError; end
 end
