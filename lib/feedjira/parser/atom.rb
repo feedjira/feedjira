4b4d803bfec9c2e7964d96f64d9c4f861614143c
Alfredo Amatriain <geralt@gmail.com>
2014-03-24 18:56:30 +0100

    When Atom parser looks for the feed URL, it looks for link elements with rel="self".
    I forgot this change in commit cd60f450d100f3637df535464131f14b4932f765.

--- lib/feedjira/parser/atom.rb
+++ lib/feedjira/parser/atom.rb
@@ -7,7 +7,7 @@ module Feedjira
       element :title
       element :subtitle, :as => :description
       element :link, :as => :url, :value => :href, :with => {:type => "text/html"}
-      element :link, :as => :feed_url, :value => :href, :with => {:type => "application/atom+xml"}
+      element :link, :as => :feed_url, :value => :href, :with => {:rel => "self"}
       elements :link, :as => :links, :value => :href
       elements :link, :as => :hubs, :value => :href, :with => {:rel => "hub"}
       elements :entry, :as => :entries, :class => AtomEntry
