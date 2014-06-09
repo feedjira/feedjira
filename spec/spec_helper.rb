ac74aef95e21afbda1a0c3f412d6431068ed5ad9
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-06-09 23:13:57 +0300

    Test both nokogiri and ox sax-machine handlers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/feedjira')
 require 'sample_feeds'
 
+if ENV['HANDLER'] == 'ox'
+  SAXMachine.handler = :ox
+end
+
 RSpec.configure do |c|
   c.include SampleFeeds
 end
