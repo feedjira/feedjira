d5f39294c5f3ab49248fe8483cd7aa21b390cb09
Christian Becker <christian.becker@empolis.com>
2016-09-06 17:47:09 +0200

    Parse last-modified response from server to Time instead of keeping the string

--- lib/feedjira/feed.rb
+++ lib/feedjira/feed.rb
@@ -72,7 +72,9 @@ module Feedjira
       feed = parse_with parser_klass, xml
       feed.feed_url = url
       feed.etag = response.headers['etag'].to_s.gsub(/"/, '')
-      feed.last_modified = response.headers['last-modified']
+
+      last_modified = response.headers['last-modified']
+      feed.last_modified = DateTime.parse(last_modified).to_time rescue nil
       feed
     end
 
