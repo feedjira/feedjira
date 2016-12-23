d83f549173df26b97101f6ef3d908ff0fafdd6aa
Michael Stock <mikeastock@gmail.com>
2016-12-22 21:38:46 -0800

    Add configurations for requests
    This allows users to configure the existing option of follow redirect
    limit as well as new options for request timeout and user agent

--- lib/feedjira/feed.rb
+++ lib/feedjira/feed.rb
@@ -2,96 +2,113 @@
 # rubocop:disable Style/DocumentationMethod
 module Feedjira
   class Feed
-    def self.parse_with(parser, xml, &block)
-      parser.parse xml, &block
-    end
-
-    def self.parse(xml, &block)
-      parser = determine_feed_parser_for_xml(xml)
-      raise NoParserAvailable, 'No valid parser for XML.' unless parser
-      parse_with parser, xml, &block
-    end
-
-    def self.determine_feed_parser_for_xml(xml)
-      start_of_doc = xml.slice(0, 2000)
-      feed_classes.detect { |klass| klass.able_to_parse?(start_of_doc) }
-    end
-
-    def self.add_feed_class(klass)
-      feed_classes.unshift klass
-    end
-
-    def self.feed_classes
-      @feed_classes ||= [
-        Feedjira::Parser::RSSFeedBurner,
-        Feedjira::Parser::GoogleDocsAtom,
-        Feedjira::Parser::AtomYoutube,
-        Feedjira::Parser::AtomFeedBurner,
-        Feedjira::Parser::Atom,
-        Feedjira::Parser::ITunesRSS,
-        Feedjira::Parser::RSS
-      ]
-    end
-
-    def self.add_common_feed_element(element_tag, options = {})
-      feed_classes.each do |k|
-        k.element element_tag, options
+    class << self
+      def parse_with(parser, xml, &block)
+        parser.parse xml, &block
       end
-    end
 
-    def self.add_common_feed_elements(element_tag, options = {})
-      feed_classes.each do |k|
-        k.elements element_tag, options
+      def parse(xml, &block)
+        parser = determine_feed_parser_for_xml(xml)
+        raise NoParserAvailable, 'No valid parser for XML.' unless parser
+        parse_with parser, xml, &block
       end
-    end
 
-    def self.add_common_feed_entry_element(element_tag, options = {})
-      call_on_each_feed_entry :element, element_tag, options
-    end
+      def determine_feed_parser_for_xml(xml)
+        start_of_doc = xml.slice(0, 2000)
+        feed_classes.detect { |klass| klass.able_to_parse?(start_of_doc) }
+      end
 
-    def self.add_common_feed_entry_elements(element_tag, options = {})
-      call_on_each_feed_entry :elements, element_tag, options
-    end
+      def add_feed_class(klass)
+        feed_classes.unshift klass
+      end
 
-    def self.call_on_each_feed_entry(method, *parameters)
-      feed_classes.each do |klass|
-        klass.sax_config.collection_elements.each_value do |value|
-          collection_configs = value.select do |v|
-            v.accessor == 'entries' && v.data_class.class == Class
-          end
+      def feed_classes
+        @feed_classes ||= [
+          Feedjira::Parser::RSSFeedBurner,
+          Feedjira::Parser::GoogleDocsAtom,
+          Feedjira::Parser::AtomYoutube,
+          Feedjira::Parser::AtomFeedBurner,
+          Feedjira::Parser::Atom,
+          Feedjira::Parser::ITunesRSS,
+          Feedjira::Parser::RSS
+        ]
+      end
 
-          collection_configs.each do |config|
-            config.data_class.send(method, *parameters)
+      def add_common_feed_element(element_tag, options = {})
+        feed_classes.each do |k|
+          k.element element_tag, options
+        end
+      end
+
+      def add_common_feed_elements(element_tag, options = {})
+        feed_classes.each do |k|
+          k.elements element_tag, options
+        end
+      end
+
+      def add_common_feed_entry_element(element_tag, options = {})
+        call_on_each_feed_entry :element, element_tag, options
+      end
+
+      def add_common_feed_entry_elements(element_tag, options = {})
+        call_on_each_feed_entry :elements, element_tag, options
+      end
+
+      def call_on_each_feed_entry(method, *parameters)
+        feed_classes.each do |klass|
+          klass.sax_config.collection_elements.each_value do |value|
+            collection_configs = value.select do |v|
+              v.accessor == 'entries' && v.data_class.class == Class
+            end
+
+            collection_configs.each do |config|
+              config.data_class.send(method, *parameters)
+            end
           end
         end
       end
-    end
 
-    def self.fetch_and_parse(url)
-      response = connection(url).get
-      unless response.success?
-        raise FetchFailure, "Fetch failed - #{response.status}"
+      def fetch_and_parse(url)
+        response = connection(url).get
+        unless response.success?
+          raise FetchFailure, "Fetch failed - #{response.status}"
+        end
+        feed = parse response.body
+        feed.feed_url = url
+        feed.etag = response.headers['etag'].to_s.delete '"'
+
+        feed.last_modified = parse_last_modified(response)
+        feed
       end
-      feed = parse response.body
-      feed.feed_url = url
-      feed.etag = response.headers['etag'].to_s.delete '"'
 
-      feed.last_modified = parse_last_modified(response)
-      feed
-    end
+      # rubocop:disable LineLength
+      def connection(url)
+        Faraday.new(url: url, headers: headers, request: request_options) do |conn|
+          conn.use FaradayMiddleware::FollowRedirects, limit: Feedjira.follow_redirect_limit
+          conn.adapter :net_http
+        end
+      end
+      # rubocop:enable LineLength
 
-    def self.connection(url)
-      Faraday.new(url: url) do |conn|
-        conn.use FaradayMiddleware::FollowRedirects, limit: 3
-        conn.adapter :net_http
+      private
+
+      def headers
+        {
+          user_agent: Feedjira.user_agent
+        }
+      end
+
+      def request_options
+        {
+          timeout: Feedjira.request_timeout
+        }
+      end
+
+      def parse_last_modified(response)
+        DateTime.parse(response.headers['last-modified']).to_time
+      rescue
+        nil
       end
     end
-
-    def self.parse_last_modified(response)
-      DateTime.parse(response.headers['last-modified']).to_time
-    rescue
-      nil
-    end
-    private_class_method :parse_last_modified
   end
 end
--- spec/feedjira/configuration_spec.rb
+++ spec/feedjira/configuration_spec.rb
@@ -2,12 +2,24 @@ require 'spec_helper'
 
 describe Feedjira::Configuration do
   describe '.configure' do
-    it 'sets strip_whitespace config' do
-      Feedjira.configure do |config|
-        config.strip_whitespace = true
-      end
+    it 'sets follow_redirect_limit config' do
+      Feedjira.configure { |config| config.follow_redirect_limit = 10 }
+      expect(Feedjira.follow_redirect_limit).to eq(10)
+    end
 
+    it 'sets request_timeout config' do
+      Feedjira.configure { |config| config.request_timeout = 45 }
+      expect(Feedjira.request_timeout).to eq(45)
+    end
+
+    it 'sets strip_whitespace config' do
+      Feedjira.configure { |config| config.strip_whitespace = true }
       expect(Feedjira.strip_whitespace).to be true
+    end
+
+    it 'sets user_agent config' do
+      Feedjira.configure { |config| config.user_agent = 'Test User Agent' }
+      expect(Feedjira.user_agent).to eq('Test User Agent')
     end
   end
 end
