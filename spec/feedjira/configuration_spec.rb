940c401e24601688b89f0ad7d0ff56ad80867e06
Michael Stock <mikeastock@gmail.com>
2017-11-18 18:45:04 -0800

    Add test for unshifting custom parser

--- spec/feedjira/configuration_spec.rb
+++ spec/feedjira/configuration_spec.rb
@@ -6,5 +6,15 @@ describe Feedjira::Configuration do
       Feedjira.configure { |config| config.strip_whitespace = true }
       expect(Feedjira.strip_whitespace).to be true
     end
+
+    it "allows parsers to be modified" do
+      original_parsers = Feedjira.parsers
+
+      CustomParser = Class.new
+
+      Feedjira.configure { |config| config.parsers.unshift(CustomParser) }
+      expect(Feedjira.parsers.first).to eq(CustomParser)
+      Feedjira.configure { |config| config.parsers = original_parsers }
+    end
   end
 end
