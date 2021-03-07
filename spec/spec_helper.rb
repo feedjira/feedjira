d315d41367ec6e6469044f4a32ad7fdcfd62376a
Matijs van Zuijlen <matijs@matijs.net>
2021-03-07 20:36:33 +0100

    Remove unused VCR dependency
    VCR is no longer actually used in the specs, since the feching portion
    of Feedjira has been removed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,8 @@
 
 require File.expand_path("#{File.dirname(__FILE__)}/../lib/feedjira")
 require "sample_feeds"
-require "vcr"
 
 SAXMachine.handler = ENV["HANDLER"].to_sym if ENV["HANDLER"]
-
-VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
-  config.hook_into :faraday
-end
 
 RSpec.configure do |c|
   c.include SampleFeeds
