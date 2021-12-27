1e7457087110c08fffbfd46a82ba6432aebf515f
Dan Gates <skynet@by-robots.com>
2021-12-27 16:59:44 +0000

    Pass `Time` offset as a fraction.
    `Time#gm` and `Time#local` no longer (tested in Ruby 3.1) seem to
    support passing offsets in the format '+01:00'. The fix for this
    is to pass the offset as a fractional part instead.

--- lib/feedjira/core_ext/date.rb
+++ lib/feedjira/core_ext/date.rb
@@ -17,6 +17,6 @@ class Date
 
   def feed_utils_to_time(dest, method)
     Time.send(method, dest.year, dest.month, dest.day, dest.hour, dest.min,
-              dest.sec, dest.zone)
+              dest.sec, dest.offset)
   end
 end
